7741d95bc715cc00020945ab616b04f422913584
techmannih <manishchaudharyttt@gmail.com>
2023-11-20 13:16:22 +0530

    array_ques=build array from permutation

---
+++ Concepts/array/array2D.java
@@ -0,0 +1,8 @@
+package Concepts.array;
+
+public class array2D {
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ Question/Array/Easy/Build_arr_from_premutation/Build_arr_from_permutation.java
@@ -0,0 +1,14 @@
+package Question.Array.Easy.Build_arr_from_premutation;
+
+public class Build_arr_from_permutation {
+    int[] nums = { 0, 2, 1, 5, 3, 4 };
+
+    int[] ans = new int[nums.length];
+
+    for( int i = 0;i<nums.length;i++)
+    {
+        ans[i] = nums[nums[i]];
+    }
+
+    return ans;
+}
