89b68d91c07750474623e0e4137b540e6bd64dad
techmannih <manishchaudharyttt@gmail.com>
2023-11-21 07:14:09 +0530

    Build_arr_from_premutation

--- Question/Array/Easy/Build_arr_from_premutation/Build_arr_from_permutation.java
+++ Question/Array/Easy/Build_arr_from_premutation/Build_arr_from_permutation.java
@@ -1,14 +1,14 @@
-package Question.Array.Easy.Build_arr_from_premutation;
+// package Question.Array.Easy.Build_arr_from_premutation;
 
-public class Build_arr_from_permutation {
-    int[] nums = { 0, 2, 1, 5, 3, 4 };
+// public class Build_arr_from_permutation {
+//     int[] nums = { 0, 2, 1, 5, 3, 4 };
 
-    int[] ans = new int[nums.length];
+//     int[] ans = new int[nums.length];
 
-    for( int i = 0;i<nums.length;i++)
-    {
-        ans[i] = nums[nums[i]];
-    }
+//     for( int i = 0;i<nums.length;i++)
+//     {
+//         ans[i] = nums[nums[i]];
+//     }
 
-    return ans;
-}
+//     return ans;
+// }
