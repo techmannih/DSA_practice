2371c817a0ea091874ede12b8c8f7e6d06c887a0
techmannih <manishchaudharyttt@gmail.com>
2023-11-24 09:54:33 +0530

    Shuffling

--- Question/Array/Easy/RichestCostumerWealth/RichestCostumerWealth.java
+++ Question/Array/Easy/RichestCostumerWealth/RichestCostumerWealth.java
@@ -3,20 +3,20 @@ package Question.Array.Easy.RichestCostumerWealth;
 public class RichestCostumerWealth {
     public static void main(String[] args) {
         int[][] accounts = { { 1, 2, 9 },
-                             { 2, 3, 4 }, };
+                { 2, 3, 4 }, };
 
-          int largeSum=0;
-          for(int i=0;i<accounts.length;i++){
-              
-            int temp =0;
-            for(int j=0;j<accounts[i].length;j++){
-                temp +=accounts[i][j];
+        int largeSum = 0;
+        for (int i = 0; i < accounts.length; i++) {
+
+            int temp = 0;
+            for (int j = 0; j < accounts[i].length; j++) {
+                temp += accounts[i][j];
 
             }
 
-        //   System.out.println();
-        largeSum=Math.max(largeSum, temp);    
-          }   
-          System.out.println(largeSum);            
+            // System.out.println();
+            largeSum = Math.max(largeSum, temp);
+        }
+        System.out.println(largeSum);
     }
 }
---
+++ Question/Array/Easy/ShuffleArray/ShuffleArray.java
@@ -0,0 +1,20 @@
+package Question.Array.Easy.ShuffleArray;
+
+import java.util.Arrays;
+
+public class ShuffleArray {
+    public static void main(String[] args) {
+
+        int[] nums = { 1, 2, 3, 4, 5, 6 }; // nums.lenght=2n
+        int n = 3;
+        int[] result = new int[(n * 2)];
+
+        for (int i = 0; i < n; i++) {
+            result[2 * i] = nums[i];
+            result[2 * i + 1] = nums[n + i];
+
+        }
+        System.out.println(Arrays.toString(result));
+    }
+
+}
